72526dd3977ca8ae130621e32667fa56de05f25b
Shivesh Kaundinya <shivesh.kaundinya@gamil.com>
2020-10-30 12:30:38 +0530

    Removed build folder from .gitignore

---
+++ client/build/precache-manifest.58a21028751dd69e38ad59d973c42f55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2b54e62551df37c20b770cb3d51093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92a4738c29f60d044ee1",
+    "url": "/static/js/2.30377d14.chunk.js"
+  },
+  {
+    "revision": "e72aa5db2c8ada42b45ae4ae98923678",
+    "url": "/static/js/2.30377d14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d668f3400aa2627d80ef",
+    "url": "/static/js/main.f492ca9e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
